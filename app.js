c36f599f83539c2f8a90d1023fe0c8217fc41f5c
Sava <sava.vuckovic@quantox.com>
2019-02-06 14:20:25 +0100

    adding data to the firestore

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const db = firebase.firestore();
 
 const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
 
 function renderCafe(doc) {
   let li = document.createElement('li');
@@ -17,9 +18,23 @@ function renderCafe(doc) {
   cafeList.appendChild(li);
 }
 
+// getting data
 db.collection('cafes')
   .get()
   .then(snapshot => {
     snapshot.docs.forEach(doc => renderCafe(doc));
   })
   .catch(err => console.log(err));
+
+// saving data
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  db.collection('cafes')
+    .add({
+      name: form.name.value,
+      city: form.city.value
+    })
+
+  form.name.value = '';
+  form.city.value = '';
+});
